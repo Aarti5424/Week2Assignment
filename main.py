0d04d32c9bf1ec6e93fcccaacb6165eb022aa1ed
Aarti <aartipathak129@gmail.com>
2021-05-18 19:46:34 -0400

    asfkm scaad

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2)
 from math import pi
 r = float(input ("Input the radius of the circle : "))
 print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
