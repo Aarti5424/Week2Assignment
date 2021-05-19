3988c9f98008f37605fc81b2eb1bb4e44c8d11e8
Aarti <aartipathak129@gmail.com>
2021-05-18 22:00:36 -0400

    display the full name by combining first and last name

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ from math import pi
 r = float(input ("Input the radius of the circle : "))
 print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
 
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ("Hello  " + lname + " " + fname)
+
+
+
+
+
 
 
